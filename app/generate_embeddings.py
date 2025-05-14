b0f6705f88706628487a1e7e9678279a2c13c714
Jerónimo Martínez <110127604+Lechuza22@users.noreply.github.com>
2025-05-14 11:31:22 -0300

    Update generate_embeddings.py

--- app/generate_embeddings.py
+++ app/generate_embeddings.py
@@ -3,7 +3,7 @@
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.document_loaders import TextLoader, PyPDFLoader
 
 DATA_DIR = "data/documentos_raw"
@@ -28,9 +28,7 @@ def create_vectorstore(documents):
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     chunks = splitter.split_documents(documents)
 
-    try:
-        embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", model_kwargs={"device": "cpu"})
-
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectorstore = FAISS.from_documents(chunks, embeddings)
     vectorstore.save_local(VECTOR_DIR)
     print(f"[✓] Vectorstore guardado en '{VECTOR_DIR}'")
